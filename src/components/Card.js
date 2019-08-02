f5455c4f37d161c5c58a8885a17f3f13b0286211
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2019-08-02 20:48:12 +0300

    Create Dynamic Robots Grid

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,10 +8,10 @@ class Card extends Component{
     render(){ 
         return( 
         <div className='bg-light-blue dib ma2 br4 grow pa3 pr3 bw2 shadow-5'>
-            <img alt='Robot Photo' src='https://robohash.org/nancy?50x150' />
+            <img alt='Robot Photo' src={`https://robohash.org/${this.props.id}?50x150`}/>
             <div className='tc'>
-                <h2 className='b'> Siri </h2>
-                <p className='i'>siri@Email.com</p>
+                <h2 className='b'> {this.props.name} </h2>
+                <p className='i'>{this.props.email}</p>
             </div>
         </div>
         
---
+++ src/robots.js
@@ -0,0 +1,98 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Mohammad',
+        username: 'MohammadAkthamObeidat',
+        email:'mohamadaktham@mail.com'
+    },
+    {
+        id: 2,
+        name: 'Mercer',
+        username: 'mercer',
+        email:'mercer@mail.com'
+    },
+    {
+        id: 3,
+        name: 'Nancy',
+        username: 'nancy',
+        email:'nancy@mail.com'
+    },
+    {
+        id: 4,
+        name: 'Alexa',
+        username: 'alexa',
+        email:'alexa@mail.com'
+    },
+    {
+        id: 5,
+        name: 'Elsa',
+        username: 'elsa',
+        email:'elsa@mail.com'
+    },
+    {
+        id: 6,
+        name: 'Ali',
+        username: 'ali',
+        email:'ali@mail.com'
+    },
+    {
+        id: 7,
+        name: 'Rami',
+        username: 'rami',
+        email:'rami@mail.com'
+    },
+    {
+        id: 8,
+        name: 'Hazard',
+        username: 'hazard',
+        email:'hazard@mail.com'
+    },
+    {
+        id: 9,
+        name: 'Ramos',
+        username: 'sergioramos',
+        email:'sergioramos@mail.com'
+    },
+    {
+        id: 10,
+        name: 'Varane',
+        username: 'rafaelvarane',
+        email:'rafaelvarane@mail.com'
+    },
+    {
+        id: 11,
+        name: 'Jack',
+        username: 'jackson',
+        email:'jackson@mail.com'
+    },
+    {
+        id: 12,
+        name: 'Navas',
+        username: 'kylorNavas',
+        email:'kylorNavas@mail.com'
+    },
+    {
+        id: 13,
+        name: 'Maria',
+        username: 'maria',
+        email:'maria@mail.com'
+    },
+    {
+        id: 14,
+        name: 'Khaled',
+        username: 'khaledRami',
+        email:'khaledRami@mail.com'
+    },
+    {
+        id: 15,
+        name: 'Mark',
+        username: 'markZ',
+        email:'markZ@mail.com'
+    },
+    {
+        id: 16,
+        name: 'Alex',
+        username: 'alex',
+        email:'alex@mail.com'
+    },
+]
