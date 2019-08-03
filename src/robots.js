615999a633e6174618bd428bdbdfba3431a14059
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2019-08-03 17:32:21 +0300

    Create 'CardList.js' Component And Make it As Parent For 'Card.js' Component

--- src/robots.js
+++ src/robots.js
@@ -1,98 +1,62 @@
 export const robots = [
     {
         id: 1,
-        name: 'Mohammad',
-        username: 'MohammadAkthamObeidat',
-        email:'mohamadaktham@mail.com'
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
     },
     {
         id: 2,
-        name: 'Mercer',
-        username: 'mercer',
-        email:'mercer@mail.com'
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv'
     },
     {
         id: 3,
-        name: 'Nancy',
-        username: 'nancy',
-        email:'nancy@mail.com'
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net'
     },
     {
         id: 4,
-        name: 'Alexa',
-        username: 'alexa',
-        email:'alexa@mail.com'
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org'
     },
     {
         id: 5,
-        name: 'Elsa',
-        username: 'elsa',
-        email:'elsa@mail.com'
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca'
     },
     {
         id: 6,
-        name: 'Ali',
-        username: 'ali',
-        email:'ali@mail.com'
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info'
     },
     {
         id: 7,
-        name: 'Rami',
-        username: 'rami',
-        email:'rami@mail.com'
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz'
     },
     {
         id: 8,
-        name: 'Hazard',
-        username: 'hazard',
-        email:'hazard@mail.com'
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me'
     },
     {
         id: 9,
-        name: 'Ramos',
-        username: 'sergioramos',
-        email:'sergioramos@mail.com'
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io'
     },
     {
         id: 10,
-        name: 'Varane',
-        username: 'rafaelvarane',
-        email:'rafaelvarane@mail.com'
-    },
-    {
-        id: 11,
-        name: 'Jack',
-        username: 'jackson',
-        email:'jackson@mail.com'
-    },
-    {
-        id: 12,
-        name: 'Navas',
-        username: 'kylorNavas',
-        email:'kylorNavas@mail.com'
-    },
-    {
-        id: 13,
-        name: 'Maria',
-        username: 'maria',
-        email:'maria@mail.com'
-    },
-    {
-        id: 14,
-        name: 'Khaled',
-        username: 'khaledRami',
-        email:'khaledRami@mail.com'
-    },
-    {
-        id: 15,
-        name: 'Mark',
-        username: 'markZ',
-        email:'markZ@mail.com'
-    },
-    {
-        id: 16,
-        name: 'Alex',
-        username: 'alex',
-        email:'alex@mail.com'
-    },
-]
+        name: 'Clementina DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz'
+    }
+];
