8131821dbb376145d785173c9cea295e6a9f5375
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2019-08-03 18:21:10 +0300

    Update CardList.js

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,13 +3,13 @@ import Card from './Card.js'
 
 const CardList = ({robots}) =>{
     
-    const cardComponent = robots.map((item, i) =>{
+    const cardArray = robots.map((item, i) =>{
        return <Card id= {robots[i].id } name={robots[i].name} email= {robots[i].email} />
     })
     
         return(
             <div>
-                {cardComponent}
+                {cardArray}
             </div>
         );
     }
